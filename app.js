f01b1f41ad2b44436b6885a8ed276269566a675f
treezrppl2 <gojiradestroy@gmail.com>
2016-03-29 20:33:42 -0700

    Moving custom modules to ./modules

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express'); var app = express();
 var request = require('request');
 var bodyParser = require('body-parser'); var jsonParser = bodyParser.json();
-var data = require('./database.js');
-var yahoo = require('./yahoo-financial');
+var data = require('./modules/database.js');
+var yahoo = require('./modules/yahoo-financial');
 yahoo.build(data);
 
 app.use(function(req, res, next) { console.log(req.method + ' : ' + req.url); next();});
-app.use(express.static('./public/'));
 
 app.post('/data/bulk', jsonParser, function(req, res) {
   var array = yahoo.current(req.body.symbols)
@@ -17,6 +16,7 @@ app.get('/data/:symbol/', function(req, res) {
   var object = yahoo.current(req.params.symbol)
   res.send(object);
 });
+app.use(express.static('./public/'));
 
 app.listen(8080, function(req, res, next) {
   console.log('Listening on: 8080...');
