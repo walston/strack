407201c307ae107338de52ec5ffda93a6cceb6de
treezrppl2 <gojiradestroy@gmail.com>
2016-04-05 11:11:11 -0700

    #5 added number of shares purchased

--- modules/userdb.js
+++ modules/userdb.js
@@ -33,41 +33,49 @@ module.exports = [
           {
             'symbol': 'ZION',
             'cost': 20.25,
+            'shares': 5,
             'fee': 7.95
           },
           {
             'symbol': 'BAC',
             'cost': 12.13,
+            'shares': 5,
             'fee': 7.95
           },
           {
             'symbol': 'FITB',
             'cost': 8.00,
+            'shares': 5,
             'fee': 7.95
           },
           {
             'symbol': 'HBAN',
             'cost': 1,
+            'shares': 5,
             'fee': 7.95
           },
           {
             'symbol': 'MTB',
             'cost': 98.98,
+            'shares': 5,
             'fee': 7.95
           },
           {
             'symbol': 'STI',
             'cost': 36.02,
+            'shares': 5,
             'fee': 7.95
           },
           {
             'symbol': 'BK',
             'cost': 35.59,
+            'shares': 5,
             'fee': 7.95
           },
           {
             'symbol': 'USB',
             'cost': 39.59,
+            'shares': 5,
             'fee': 7.95
           }
         ]
