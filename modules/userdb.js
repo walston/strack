76d8673b50dc3c9a8eac99fd3384c54119aad003
treezrppl2 <gojiradestroy@gmail.com>
2016-04-05 11:04:33 -0700

    #5 Purchaselist added to user for testing

--- modules/userdb.js
+++ modules/userdb.js
@@ -25,6 +25,53 @@ module.exports = [
           'AAPL'
         ]
       }
+    ],
+    'purchaselists': [
+      {
+        'name': 'banking',
+        'stocks': [
+          {
+            'symbol': 'ZION',
+            'cost': 20.25,
+            'fee': 7.95
+          },
+          {
+            'symbol': 'BAC',
+            'cost': 12.13,
+            'fee': 7.95
+          },
+          {
+            'symbol': 'FITB',
+            'cost': 8.00,
+            'fee': 7.95
+          },
+          {
+            'symbol': 'HBAN',
+            'cost': 1,
+            'fee': 7.95
+          },
+          {
+            'symbol': 'MTB',
+            'cost': 98.98,
+            'fee': 7.95
+          },
+          {
+            'symbol': 'STI',
+            'cost': 36.02,
+            'fee': 7.95
+          },
+          {
+            'symbol': 'BK',
+            'cost': 35.59,
+            'fee': 7.95
+          },
+          {
+            'symbol': 'USB',
+            'cost': 39.59,
+            'fee': 7.95
+          }
+        ]
+      }
     ]
   }
 ]
